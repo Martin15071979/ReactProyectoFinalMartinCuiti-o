fce90d828ce5c5ce14a25d4f63cf0e94f2db6ca3
Martin <martincuitino@gmail.com>
2024-12-12 21:15:11 -0300

    EntregaFinalReactMartinCuitino

---
+++ trabajo-final/src/data.js
@@ -0,0 +1,44 @@
+export const productosJoyería = [
+	{
+	  id: 1,
+	  imagen: "https://example.com/imagen1.jpg",
+	  descripcion: "Anillo de plata con zirconia, elegante y sofisticado.",
+	  precio: 150,
+	  genero: "Unisex"
+	},
+	{
+	  id: 2,
+	  imagen: "https://example.com/imagen2.jpg",
+	  descripcion: "Collar de oro de 18k con diamantes, para ocasiones especiales.",
+	  precio: 1200,
+	  genero: "Femenino"
+	},
+	{
+	  id: 3,
+	  imagen: "https://example.com/imagen3.jpg",
+	  descripcion: "Pulsera de acero inoxidable con detalles en negro.",
+	  precio: 80,
+	  genero: "Masculino"
+	},
+	{
+	  id: 4,
+	  imagen: "https://example.com/imagen4.jpg",
+	  descripcion: "Pendientes de perlas con base de oro blanco, elegantes y modernos.",
+	  precio: 250,
+	  genero: "Femenino"
+	},
+	{
+	  id: 5,
+	  imagen: "https://example.com/imagen5.jpg",
+	  descripcion: "Reloj de acero inoxidable con correa de cuero, ideal para el día a día.",
+	  precio: 180,
+	  genero: "Masculino"
+	},
+	{
+	  id: 6,
+	  imagen: "https://example.com/imagen6.jpg",
+	  descripcion: "Anillo de compromiso de platino con diamante central.",
+	  precio: 3000,
+	  genero: "Femenino"
+	}
+  ];
---
+++ trabajo-final/src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTjGQNhyMzUwhAa2hz-pcYQS4SwfzE8a0",
+  authDomain: "entrega-react-martin-c.firebaseapp.com",
+  projectId: "entrega-react-martin-c",
+  storageBucket: "entrega-react-martin-c.firebasestorage.app",
+  messagingSenderId: "680212623764",
+  appId: "1:680212623764:web:f7a35222db956fe9605f81"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+  </>
+)
