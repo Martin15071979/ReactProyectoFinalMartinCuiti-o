0b67e505f55fec0a2a2b99812cc702d5e02cf6b8
Martin <martincuitino@gmail.com>
2025-01-08 20:43:07 -0300

    config changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 	return (
 		<>
 		<Provider>
-			<BrowserRouter>
+			<BrowserRouter basename='/ReactProyectoFinalMartinCuitino/'>
 				<Header
 					allProducts={allProducts}
 					setAllProducts={setAllProducts}
