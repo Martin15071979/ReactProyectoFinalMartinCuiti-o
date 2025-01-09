27fd35fa3fea2871e7e677245a03f4f3352eb2a6
Martin <martincuitino@gmail.com>
2025-01-08 21:57:09 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 	return (
 		<>
 		<Provider>
-			<BrowserRouter basename='/ReactProyectoFinalMartinCuitino/'>
+			<BrowserRouter basename='/ReactProyectoFinalMartinCuitino'>
 				<Header
 					allProducts={allProducts}
 					setAllProducts={setAllProducts}
